26a206b816d7ee801ab81e4a3b63feb73cdab3d0
Oláh Tamás <tolah94@gmail.com>
2017-11-22 15:23:16 +0100

    Extracted 2 methods from score() function, which are logically separable

--- bowling.py
+++ bowling.py
@@ -8,24 +8,34 @@ def score(game):
         else:
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
+            result = handle_bonus_points(game, i, result)
         last_frame_half = get_value(game[i])
-        if not in_first_half_of_frame:
-            frame += 1
-        if in_first_half_of_frame is True:
-            in_first_half_of_frame = False
+        frame, in_first_half_of_frame = decide_which_half_of_frame(frame, in_first_half_of_frame, game, i)
+    return result
+
+
+def decide_which_half_of_frame(frame, in_first_half_of_frame, game, i):
+    if not in_first_half_of_frame:
+        frame += 1
+    if in_first_half_of_frame is True:
+        in_first_half_of_frame = False
+    else:
+        in_first_half_of_frame = True
+    if game[i] == 'X' or game[i] == 'x':
+        in_first_half_of_frame = True
+        frame += 1
+    return frame, in_first_half_of_frame
+
+
+def handle_bonus_points(game, i, result):
+    if game[i] == '/':
+        result += get_value(game[i+1])
+    elif game[i] == 'X' or game[i] == 'x':
+        result += get_value(game[i+1])
+        if game[i+2] == '/':
+            result += 10 - get_value(game[i+1])
         else:
-            in_first_half_of_frame = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half_of_frame = True
-            frame += 1
+            result += get_value(game[i+2])
     return result
 
 
