2f59cbb99383e0494ad2369f1ab3d8bf86c63d8f
Oláh Tamás <tolah94@gmail.com>
2017-11-22 17:12:23 +0100

    Renamed result variable to score for better understanding

--- bowling.py
+++ bowling.py
@@ -3,22 +3,22 @@ def score(game):
         "X" indicates strike, "/" indicates spare, "-" indicates miss.
         Example: "1/35XXX458/X3/XX6" = 189 points
         Returns: The score of the string (int)'''
-    result = 0
+    score = 0
     frame = 1
     in_first_half_of_frame = True
     for i in range(len(game)):
-        # Adds the value of the character to result:
+        # Adds the value of the character to score:
         if game[i] == '/':
             last_frame_half = get_value(game[i-1])
-            result += 10 - last_frame_half
+            score += 10 - last_frame_half
         else:
-            result += get_value(game[i])
-        # If it's a strike or a spare adds the bonus points to result:
+            score += get_value(game[i])
+        # If it's a strike or a spare adds the bonus points to score:
         if frame < 10 and get_value(game[i]) == 10:
-            result = handle_bonus_points(game, i, result)
+            score = handle_bonus_points(game, i, score)
         # Keeps track of the rounds (frame):
         frame, in_first_half_of_frame = frame_counter(frame, in_first_half_of_frame, game[i])
-    return result
+    return score
 
 
 def frame_counter(frame, in_first_half_of_frame, char):
@@ -37,20 +37,20 @@ def frame_counter(frame, in_first_half_of_frame, char):
     return frame, in_first_half_of_frame
 
 
-def handle_bonus_points(game, i, result):
+def handle_bonus_points(game, i, score):
     '''Handles the bonus points:
         -"/" (spare): 10 + the score of the next round.
         -"X" (strike): 10 + the score of the next 2 rounds
-        Returns: The result with the bonus points added to it. (int)'''
+        Returns: The score with the bonus points added to it. (int)'''
     if game[i] == '/':
-        result += get_value(game[i+1])
+        score += get_value(game[i+1])
     elif game[i] == 'X' or game[i] == 'x':
-        result += get_value(game[i+1])
+        score += get_value(game[i+1])
         if game[i+2] == '/':
-            result += 10 - get_value(game[i+1])
+            score += 10 - get_value(game[i+1])
         else:
-            result += get_value(game[i+2])
-    return result
+            score += get_value(game[i+2])
+    return score
 
 
 def get_value(char):
