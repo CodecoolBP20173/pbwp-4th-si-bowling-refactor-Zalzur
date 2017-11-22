f278647ac4daa3e817c15b860ede7fdc89a970a9
Oláh Tamás <tolah94@gmail.com>
2017-11-22 17:02:17 +0100

    Added documentation blocks and comments for better and faster understanding

--- bowling.py
+++ bowling.py
@@ -1,20 +1,30 @@
 def score(game):
+    '''Transforms a given string to score, according to the scoring of bowling.
+        "X" indicates strike, "/" indicates spare, "-" indicates miss.
+        Example: "1/35XXX458/X3/XX6" = 189 points
+        Returns: The score of the string (int)'''
     result = 0
     frame = 1
     in_first_half_of_frame = True
     for i in range(len(game)):
+        # Adds the value of the character to result:
         if game[i] == '/':
             last_frame_half = get_value(game[i-1])
             result += 10 - last_frame_half
         else:
             result += get_value(game[i])
+        # If it's a strike or a spare adds the bonus points to result:
         if frame < 10 and get_value(game[i]) == 10:
             result = handle_bonus_points(game, i, result)
+        # Keeps track of the rounds (frame):
         frame, in_first_half_of_frame = frame_counter(frame, in_first_half_of_frame, game[i])
     return result
 
 
 def frame_counter(frame, in_first_half_of_frame, char):
+    '''Trace which frame we are in and which half of it.
+        Returns: -The number of the frame we are in (int).
+                 -True, if we are in the first half. False, if the second half (Boolean).'''
     if not in_first_half_of_frame:
         frame += 1
     if in_first_half_of_frame is True:
@@ -28,6 +38,10 @@ def frame_counter(frame, in_first_half_of_frame, char):
 
 
 def handle_bonus_points(game, i, result):
+    '''Handles the bonus points:
+        -"/" (spare): 10 + the score of the next round.
+        -"X" (strike): 10 + the score of the next 2 rounds
+        Returns: The result with the bonus points added to it. (int)'''
     if game[i] == '/':
         result += get_value(game[i+1])
     elif game[i] == 'X' or game[i] == 'x':
@@ -40,6 +54,11 @@ def handle_bonus_points(game, i, result):
 
 
 def get_value(char):
+    '''Gives back the value of the given character (this given character must be a string).
+        "X" and "/" worth 10 points.
+        "-" worths 0 point.
+        The numbers worth equal as themselves.
+        Returns: The value of the given character (int)'''
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
